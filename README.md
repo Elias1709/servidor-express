7bf4a4dc34986d7a12bee3931c2e6b1c7372af7b
Elias <eliias1709gmail.com>
2023-07-31 00:53:49 -0500

    Autenticacion implementada

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 #se crea la tercera rama al proyecto
 #se crea el middleware error-handler para manejar los errores de los metodos http
 #se crea el middleware validarMetodoHttp y validarParametros
+#Se completa el laboratorio de autenticacion exitosamente
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
+
+const secretKey = process.env.SECRET_KEY;
 const validarMetodoHTTP = require('./validarMetodoHTTP');
 
 const listViewRouter = require('./list-view-router');
@@ -14,6 +19,46 @@ app.use(validarMetodoHTTP);
 app.use('/tareas', listViewRouter);
 
 app.use('/tareas', listEditRouter);
+
+const users = [
+  { id: 1, username: 'usuario1', password: '1234' },
+  { id: 2, username: 'usuario2', password: '9876' },
+  
+];
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  const user = users.find(u => u.username === username && u.password === password);
+
+  if (!user) {
+    return res.status(401).json({ error: 'Credenciales inválidas' });
+  }
+
+  const token = jwt.sign({ userId: user.id, username: user.username }, secretKey, { expiresIn: '1h' });
+
+  res.json({ token });
+});
+
+function verificarToken(req, res, next) {
+  const token = req.headers.authorization?.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ error: 'Token no proporcionado' });
+  }
+
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ error: 'Token inválido' });
+    }
+    req.user = decoded;
+    next();
+  });
+}
+
+app.get('/ruta-protegida', verificarToken, (req, res) => {
+  res.json({ message: 'Ruta protegida alcanzada', user: req.user });
+});
 
 const taskList = [
   {
